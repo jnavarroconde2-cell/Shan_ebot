f91b272b82d3856a591009a13d63be4dc00da8e1
Josef justidiano Navarro conde <jnavarroconde2@gmail.com>
2026-07-14 18:31:42 -0500

    Refactor bot commands and enhance user interaction
    Updated bot commands and responses to provide a menu and additional information.

--- bot.py
+++ bot.py
@@ -1,18 +1,39 @@
 import telebot
 import os
+
 TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Hola! Soy Shan_ebot 🤖\n¿Qué necesitas?")
+def start(msg):
+    bot.reply_to(msg, "Hola! Soy @Shan_ebot 🤖\n\nUsa /menu para ver todos los comandos que tengo")
 
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id, "/start - Iniciar\n/help - Ayuda")
+@bot.message_handler(commands=['menu'])
+def menu(msg):
+    texto = """**MENU DE COMANDOS**
 
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    bot.send_message(message.chat.id, "Me dijiste: " + message.text)
+start - Iniciar el bot
+menu - Ver todos los comandos
+info - Info del bot
+ayuda - Ayuda
+hola - Saludo
+"""
+    bot.reply_to(msg, texto, parse_mode="Markdown")
 
-bot.polling(none_stop=True)
+@bot.message_handler(commands=['info'])
+def info(msg):
+    bot.reply_to(msg, "Soy @Shan_ebot\nCreado por ti\nActivo 24/7 en Railway ✅")
+
+@bot.message_handler(commands=['ayuda'])
+def ayuda(msg):
+    bot.reply_to(msg, "¿Necesitas ayuda? Usa /menu para ver todos los comandos disponibles")
+
+@bot.message_handler(commands=['hola'])
+def hola(msg):
+    bot.reply_to(msg, "Hooola 👋 ¿Cómo estás?")
+
+@bot.message_handler(func=lambda m: True)
+def eco(msg):
+    bot.reply_to(msg, f"Recibí: {msg.text}\nUsa /menu para ver comandos")
+
+bot.infinity_polling()
