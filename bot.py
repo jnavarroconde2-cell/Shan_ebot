d850bc85e5708f985227f939225a3f951262c6c4
Josef justidiano Navarro conde <jnavarroconde2@gmail.com>
2026-07-14 18:49:20 -0500

    Enhance bot commands and add new functionalities
    Updated command descriptions and added new commands for utilities and owner contact.

--- bot.py
+++ bot.py
@@ -12,16 +12,16 @@ def start(msg):
 def menu(msg):
     texto = """MENU DE COMANDOS
 
-General: 
-/start
-/menu
-/info
+General:
+/start - Iniciar el bot
+/menu - Ver todos los comandos
+/info - Info del bot
+/ayuda - Ayuda
 
-Ayuda:
-/ayuda
-
-Saludos:
-/hola"""
+Utilidades:
+/hola - Saludo
+/ping - Ver si el bot responde
+/owner - Contacto del creador del bot"""
     bot.reply_to(msg, texto)
 
 @bot.message_handler(commands=['info'])
@@ -34,7 +34,15 @@ def ayuda(msg):
 
 @bot.message_handler(commands=['hola'])
 def hola(msg):
-    bot.reply_to(msg, "Me dijiste: /hola")
+    bot.reply_to(msg, "Hola 👋 ¿Cómo estás?")
+
+@bot.message_handler(commands=['ping'])
+def ping(msg):
+    bot.reply_to(msg, "Pong! 🏓 El bot está activo")
+
+@bot.message_handler(commands=['owner'])
+def owner(msg):
+    bot.reply_to(msg, "Contacto del creador del bot:\n929 207 065")
 
 @bot.message_handler(func=lambda m: True)
 def eco(msg):
